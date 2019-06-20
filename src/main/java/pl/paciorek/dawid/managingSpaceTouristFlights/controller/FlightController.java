a0ee04e2b2b54a69966d3d9332e695c4bfc78153
Dawid24 <paciorekdawidonly@gmail.com>
2019-06-20 20:37:05 +0200

    implements crud methods

---
+++ src/main/java/pl/paciorek/dawid/managingSpaceTouristFlights/controller/FlightController.java
@@ -0,0 +1,65 @@
+package pl.paciorek.dawid.managingSpaceTouristFlights.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import pl.paciorek.dawid.managingSpaceTouristFlights.model.Flight;
+import pl.paciorek.dawid.managingSpaceTouristFlights.model.User;
+import pl.paciorek.dawid.managingSpaceTouristFlights.service.FlightService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class FlightController {
+
+    private FlightService flightService;
+
+    @Autowired
+    public void setFlightService(FlightService flightService) {
+        this.flightService = flightService;
+    }
+
+    @GetMapping("/add_flight")
+    public String add_spaceFlight(Model model) {
+        model.addAttribute("flight", new Flight());
+        return "flight_form";
+    }
+
+    @PostMapping("/add_flight")
+    public String addFlight(@ModelAttribute @Valid Flight flight,
+                          BindingResult bindResult) {
+        if(bindResult.hasErrors())
+            return "flight_form";
+        else {
+            flightService.save(flight);
+            return "index";
+        }
+    }
+
+    @RequestMapping("/search_flight")
+    public ModelAndView getFlights() {
+        ModelAndView modelAndView = new ModelAndView("search_flight");
+        List<Flight> flightsList = flightService.listAll();
+        modelAndView.addObject("flightsList", flightsList);
+        return modelAndView;
+    }
+
+    @RequestMapping("edit_f")
+    public ModelAndView editFlightForm(@RequestParam long id) {
+        ModelAndView modelAndView = new ModelAndView("edit_flight");
+        Flight flight = flightService.get(id);
+        modelAndView.addObject("flight", flight);
+        return modelAndView;
+    }
+
+    @RequestMapping("/delete_f")
+    public String deleteFlight(@RequestParam long id) {
+        flightService.delete(id);
+
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/pl/paciorek/dawid/managingSpaceTouristFlights/service/FlightService.java
@@ -0,0 +1,37 @@
+package pl.paciorek.dawid.managingSpaceTouristFlights.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.paciorek.dawid.managingSpaceTouristFlights.model.Flight;
+import pl.paciorek.dawid.managingSpaceTouristFlights.repository.FlightRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FlightService {
+
+    private FlightRepository flightRepository;
+
+    @Autowired
+    public void setFlightRepository(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
+    public void save(Flight flight) {
+        flightRepository.save(flight);
+    }
+
+    public List<Flight> listAll() {
+        return (List<Flight>) flightRepository.findAll();
+    }
+
+    public Flight get(Long id) {
+        Optional<Flight> result = flightRepository.findById(id);
+        return result.get();
+    }
+
+    public void delete(Long id) {
+        flightRepository.deleteById(id);
+    }
+}
